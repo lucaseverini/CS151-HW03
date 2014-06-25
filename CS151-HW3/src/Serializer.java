bcc0c80f11cb8b111323c5dcb3f613bc640f2785
Luca Severini <lucaseverini@mac.com>
2014-06-24 17:25:35 -0700

    Added Serializer class.

---
+++ CS151-HW3/src/Serializer.java
@@ -0,0 +1,21 @@
+/*
+	Serializer.java
+
+    Assignment #3 - CS151 - SJSU
+	By Luca Severini, Omari Straker, Syed Sarmad, Matt Szikley
+	June-24-2014
+*/
+
+// Serializer -----------------------------------------------------
+public class Serializer 
+{
+	static int saveSlideToFile(Object o, String filePath)
+	{
+		return 0;
+	}
+	
+	static Object openSlideToFile(String filePath)
+	{
+		return null;
+	}
+}
