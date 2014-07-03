0f8b2591cccd088085412048e88d0ec019a3d4ad
Strakaman <straka3000@gmail.com>
2014-07-02 18:41:25 -0700

    added getSize method

--- CS151-HW3/src/SlideShow.java
+++ CS151-HW3/src/SlideShow.java
@@ -157,4 +157,9 @@ public class SlideShow implements Serializable
 	{
 		return "SlideShow " + name + " : " + getImages().size() + " slide(s)";
 	}
+        
+        public int getSize()
+        {
+            return images.size();
+        }
 }
